eb7e3e8d783fa81ba7c2aed2e8e9e24d14186b3e
SnyDong <2294768134@qq.com>
2019-05-16 09:31:08 +0800

    学生实体类初始化工程

---
+++ Student_NNXY/src/com/nnxy/student/Student.java
@@ -0,0 +1,46 @@
+package com.nnxy.student;
+
+public class Student {
+	
+	private int id;
+	private int age;
+	private String sex;
+	private String name;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((sex == null) ? 0 : sex.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (age != other.age)
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (sex == null) {
+			if (other.sex != null)
+				return false;
+		} else if (!sex.equals(other.sex))
+			return false;
+		return true;
+	}
+	
+	
+}
